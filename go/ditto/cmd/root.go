1775924a5487c0d1fd81c436066328686e4c98b2
10hin <10hin@outlook.jp>
2022-02-08 01:20:46 +0900

    implement config file loading

--- go/ditto/cmd/root.go
+++ go/ditto/cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.10h.in/ditto/cli/pkg/ditto/config"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,6 +17,20 @@ var (
 		Long:  "Eclipse Ditt(TM) CLI tool to Manage and Operate",
 		Run: func(cmd *cobra.Command, args []string) {
 			// TODO
+			var err error
+			var cfg *config.DittoConfig
+			allSettings := viper.AllSettings()
+			cfg, err = config.Parse(allSettings)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			var cfgBytes []byte
+			cfgBytes, err = json.Marshal(cfg)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			// TODO: remove following debug log
+			log.Printf("server.http.url_prefix: %s\n", (string)(cfgBytes))
 			log.Panicln("Not implemented yet")
 		},
 	}
@@ -22,26 +38,37 @@ var (
 )
 
 func Execute() {
-	log.Println(viper.GetString("hoge"))
-	err := rootCmd.Execute()
+	var err error
+
+	err = rootCmd.Execute()
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "c", "config file (default is $HOME/.config/ditto-cli/config.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.config/ditto-cli/config.yaml)")
+
 	rootCmd.AddCommand(twinCmd)
 }
 
 func initConfig() {
+	var err error
 	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
+		viper.SetConfigType("yaml")
 	} else {
-		configDir, err := os.UserConfigDir()
-		cobra.CheckErr(err)
+		var configDir string
+		home, ok := os.LookupEnv("HOME")
+		if ok {
+			configDir = home + "/.config"
+		} else {
+			configDir, err = os.UserConfigDir()
+			cobra.CheckErr(err)
+		}
 
 		dittoCliDir := filepath.FromSlash(configDir + "/ditto-cli")
 		viper.AddConfigPath(dittoCliDir)
@@ -52,7 +79,7 @@ func initConfig() {
 	viper.SetEnvPrefix("dittocli")
 	viper.AutomaticEnv()
 
-	err := viper.ReadInConfig()
+	err = viper.ReadInConfig()
 	if err != nil {
 		log.Panicln(err)
 	}
---
+++ go/pkg/ditto/config/config.go
@@ -0,0 +1,106 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func Parse(raw map[string]interface{}) (*DittoConfig, error) {
+	var err error
+
+	jsonliteral := &bytes.Buffer{}
+	err = json.NewEncoder(jsonliteral).Encode(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	parsed := &DittoConfig{}
+	err = json.NewDecoder(jsonliteral).Decode(parsed)
+	if err != nil {
+		return nil, err
+	}
+
+	return parsed, nil
+}
+
+type DittoConfig struct {
+	Server *ServerConfig `json:"server"`
+}
+
+type ServerConfig struct {
+	// TODO: implement
+	HTTP      *HTTPConfig      `json:"http,omitempty"`
+	Websocket *WebsocketConfig `json:"websocket,omitempty"`
+	MQTT      *MQTTConfig      `json:"mqtt,omitempty"`
+	AMQP      *AMQPConfig      `json:"amqp,omitempty"`
+	Kafka     *KafkaConfig     `json:"kafka,omitempty"`
+}
+
+type HTTPConfig struct {
+	// TODO: implement
+	URLPrefix string           `json:"url_prefix"`
+	Basic     *BasicAuthConfig `json:"basic,omitempty"`
+	TLS       *TLSConfig       `json:"tls,omitempty"`
+}
+
+type WebsocketConfig struct {
+	// TODO: implement
+	URL   string           `json:"url"`
+	Basic *BasicAuthConfig `json:"basic,omitempty"`
+	TLS   *TLSConfig       `json:"tls,omitempty"`
+}
+
+type MQTTConfig struct {
+	// TODO: implement
+	Host            string           `json:"host"`
+	Port            uint16           `json:"port"`
+	Basic           *BasicAuthConfig `json:"basic,omitempty"`
+	EnableWebsocket bool             `json:"enable_websocket,omitempty"`
+	TLS             *TLSConfig       `json:"tls,omitempty"`
+}
+
+type AMQPConfig struct {
+	// TODO: implement
+	Host  string           `json:"host"`
+	Port  uint16           `json:"port"`
+	Basic *BasicAuthConfig `json:"basic,omitempty"`
+	TLS   *TLSConfig       `json:"tls,omitempty"`
+}
+
+type KafkaConfig struct {
+	// TODO: implement
+	Host string     `json:"host"`
+	Port uint16     `json:"port"`
+	TLS  *TLSConfig `json:"tls,omitempty"`
+}
+
+type BasicAuthConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type TLSConfig struct {
+	CA     *CAConfig         `json:"ca,omitempty"`
+	Client *ClientCertConfig `json:"client,omitempty"`
+}
+
+type CAConfig struct {
+	CertPath string `json:"cert_path,omitempty"`
+	Cert     string `json:"cert,omitempty"`
+}
+
+type ClientCertConfig struct {
+	CA   *CAConfig       `json:"ca,omitempty"`
+	Path *ClientCertPath `json:"path,omitempty"`
+	Data *ClientCertData `json:"data,omitempty"`
+}
+
+type ClientCertPath struct {
+	Cert string `json:"cert,omitempty"`
+	Key  string `json:"key,omitempty"`
+}
+
+type ClientCertData struct {
+	Cert string `json:"cert,omitempty"`
+	Key  string `json:"key,omitempty"`
+}
